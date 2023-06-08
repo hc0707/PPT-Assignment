0fe243220375beaf3e03048ca30d6fa271b71c4a
Harsh Chauhan <115972653+hc0707@users.noreply.github.com>
2023-06-08 17:38:13 +0530

    Assignment 7 - Strings

---
+++ Assignment 7 - Strings/Assignment5.java
@@ -0,0 +1,32 @@
+package assignement;
+
+public class Assignment5 {
+	public static String reverseStr(String s, int k) {
+		char[] arr = s.toCharArray();
+		int n = arr.length;
+
+		for (int i = 0; i < n; i += 2 * k) {
+			int start = i;
+			int end = Math.min(i + k - 1, n - 1);
+
+			while (start < end) {
+				char temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+				start++;
+				end--;
+			}
+		}
+
+		return new String(arr);
+	}
+
+	public static void main(String[] args) {
+		String s = "abcdefg";
+		int k = 2;
+
+		String reversed = reverseStr(s, k);
+
+		System.out.println("Output: " + reversed);
+	}
+}
---
+++ Assignment 7 - Strings/Assignment6.java
@@ -0,0 +1,22 @@
+package assignement;
+
+public class Assignment6 {
+	public static boolean canShift(String s, String goal) {
+		if (s.length() != goal.length()) {
+			return false;
+		}
+
+		String doubledS = s + s;
+
+		return doubledS.contains(goal);
+	}
+
+	public static void main(String[] args) {
+		String s = "abcde";
+		String goal = "cdeab";
+
+		boolean canShift = canShift(s, goal);
+
+		System.out.println("Output: " + canShift);
+	}
+}
